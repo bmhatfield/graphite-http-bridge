c337f7b966b04605d3d7b5b32c2b7b789d3d2bc3
Brian Hatfield <bhatfield@brightcove.com>
2013-04-11 22:28:50 -0400

    Improve socket handling, and re-enqueue events if sending fails.

--- graphite-http-bridge.py
+++ graphite-http-bridge.py
@@ -22,29 +22,39 @@ class GraphiteSender(threading.Thread):
         self.port = port
         self.udp = udp
         self.batch = 250
-        self.last_sent = 0
-        self.send_every = 60
+        self.last_sent = time.time()
+        self.send_every = 10
+        self.timeout = 5.0
         self.enabled = True
         self.sendnow = False
 
-        self.socket = self.connect()
+        self.connect()
 
     def run(self):
         while self.enabled:
-            if self.queue.qsize() > self.batch or self.sendnow:
+            if self.queue.qsize() > self.batch or (self.send_overdue() and not self.queue.empty()):
                 batch = []
                 while not self.queue.empty():
                     batch.append(self.queue.get())
 
                 if len(batch) > 0:
-                    self.send(self.pickled(batch))
-                    self.last_sent = time.time()
+                    print "Found metric batch with %s metrics" % (len(batch))
+
+                    try:
+                        self.send(self.pickled(batch))
+                        self.last_sent = time.time()
+                    except:
+                        print "Failed to send: re-enqueueing %s metrics" % (len(batch))
+                        for metric in batch:
+                            self.queue.put(metric)
+                        time.sleep(self.send_every)
+                else:
                     self.sendnow = False
             else:
-                if time.time() - self.last_sent > 60:
-                    self.sendnow = True
-                else:
-                    time.sleep(1)
+                time.sleep(1)
+
+    def send_overdue(self):
+        return time.time() - self.last_sent > self.send_every
 
     def pickled(self, tuples):
         payload = pickle.dumps(tuples)
@@ -52,26 +62,40 @@ class GraphiteSender(threading.Thread):
         return header + payload
 
     def connect(self):
-        # TODO: Smoother connection handling
         if self.udp:
             adapter = socket.SOCK_DGRAM
         else:
             adapter = socket.SOCK_STREAM
 
-        sock = socket.socket(socket.AF_INET, adapter)
+        self.socket = socket.socket(socket.AF_INET, adapter)
+        self.socket.settimeout(self.timeout)
 
-        if sock is not None:
-            sock.connect((self.host, self.port))
-            return sock
+        if self.socket is not None:
+            self.socket.connect((self.host, self.port))
         else:
-            raise IOError("Unable to connect to Graphite")
+            raise IOError("Unable to create socket")
+
+    def close(self):
+        if self.socket is not None:
+            self.socket.close()
+        self.socket = None
 
     def send(self, data):
         # TODO: Handle connection errors!
-        if self.socket is None:
-            self.socket = self.connect()
+        try:
+            if self.socket is None:
+                self.connect()
             
-        self.socket.sendall(data)
+            if self.socket is not None:
+                print "Sending pickled data..."
+                self.socket.sendall(data)
+                print "Sent."
+            else:
+                print "Socket unavailable."
+        except Exception as e:
+            print "Send failed", e
+            self.close()
+            raise e
 
 
 @app.post('/publish/:api_key')
@@ -88,6 +112,7 @@ def publish(api_key):
                 bottle.abort(400, "JSON Missing Value: %s" % (param))                
 
         try:
+            print "Queueing metric: %s" % (params)
             q.put((params['metric'], (params['timestamp'], params['value'])))
         except Exception as e:
             print e
@@ -101,7 +126,11 @@ if __name__ == "__main__":
     valid_api_keys = ['af092bfcc9302']
 
     # TODO: Parameters / optparse
-    q = Queue.Queue(maxsize=250000)
-    sender_thread = GraphiteSender(q, '127.0.0.1', 2004)
-    sender_thread.start()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    try:
+        q = Queue.Queue(maxsize=250000)
+        sender_thread = GraphiteSender(q, '127.0.0.1', 2004)
+        sender_thread.start()
+        app.run(host='0.0.0.0', port=8080, debug=True)
+    except Exception as e:
+        print "Startup failed..."
+        print e
