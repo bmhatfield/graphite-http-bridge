2fb330155e020eb8a1d6182a16c619503b109ea5
Brian Hatfield <bmhatfield@gmail.com>
2013-04-18 19:46:38 -0400

    Add support for packaging graphite-http-bridge

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+version = "0.1.0"
+
+setup(name="graphite-http-bridge",
+      version=version,
+      description="Bottle-based HTTP bridge to Graphite",
+      author="Brian Hatfield",
+      author_email="bmhatfield@gmail.com",
+      url="https://github.com/bmhatfield/graphite-http-bridge",
+      package_dir={'': 'lib'},
+      py_modules=['apiauth', 'graphitesender', 'metric'],
+      data_files=[('/etc/init/', ["init/ubuntu/graphite-http-bridge.conf"])],
+      scripts=["graphite-http-bridge.py"]
+    )
